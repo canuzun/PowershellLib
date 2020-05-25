a6439c99ea10966891387ebf37fea9b298b63ab1
Can Uzun <73012@grundfos.com>
2020-05-25 10:00:23 +0200

    win2019 with sql2019 image option added. username and password option added.  -ErrorAction Stop added to New-* File renamed.

--- AzureNewVM.ps1
+++ AzureNewVM.ps1
@@ -3,15 +3,27 @@
 #Get-AzVMImageSku -Location 'West Europe' -PublisherName 'MicrosoftWindowsServer' -Offer 'WindowsServer' | Select Skus
 ##
 ##
+
 param(
 $location,
 $resourceGroupName,
 $vmName,
 $type,
+$vmUsername,
+$vmPw,
 $stopVM
 )
 
-if($type -ne "win10" -and $type -ne "ubuntu" -and $type -ne "win7" -and $type -ne "server2016")
+#& 'C:\GitHub-repo\PowershellLib\AzureNewVM.ps1' "North Europe" GF-RG-NutShell-T-TestClients vm-wsql1 winsql2019 cuzun 0MMz!Q4J2gD5 False
+# $location = "North Europe"
+# $resourceGroupName = "GF-RG-NutShell-T-TestClients"
+# $vmName = "vm-winsqlt1"
+# $type = "winsql2019"
+# $stopVM = $false
+# $vmUsername = "cuzun"
+# $vmPw = "0MMz!Q4J2gD5"
+
+if($type -ne "win10" -and $type -ne "ubuntu" -and $type -ne "win7" -and $type -ne "server2016" -and $type -ne "winsql2019")
 {
     Write-Host "Invalid type" : $type
     return
@@ -23,9 +35,6 @@ $stopWatch.Start()
 Write-Host "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] : Starting"
 #Get-AzSubscription
 
-$vmUsername = "cuzun"
-$vmPw = "0MMz!Q4J2gD5"
-#$location = "West Europe"
 $vMSize = "Standard_D2"
 if($type -eq "server2016")
 {
@@ -40,7 +49,7 @@ $resourceGroup = Get-AzResourceGroup -Name $resourceGroupName -ErrorAction Silen
 if($null -eq $resourceGroup)
 {
     Write-Host "Resource Group not found, creating RG :" $resourceGroupName
-    $resourceGroup = New-AzResourceGroup -Name $resourceGroupName -Location "westeurope"
+    $resourceGroup = New-AzResourceGroup -Name $resourceGroupName -Location $location -ErrorAction Stop
 }
 
 $networkName = $resourceGroupName+"-vnet"
@@ -51,27 +60,27 @@ if($null -eq $vnet)
     $ip2 = Get-Random -Minimum 1 -Maximum 240
     $addressPrefix = "10.$ip1.$ip2.0/24"
     $subnetPrefix = "10.$ip1.$ip2.0/27"
-    $subnet = New-AzVirtualNetworkSubnetConfig -Name "default" -AddressPrefix $subnetPrefix
-    $vnet = New-AzVirtualNetwork -Name $networkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $addressPrefix -Subnet $subnet
+    $subnet = New-AzVirtualNetworkSubnetConfig -Name "default" -AddressPrefix $subnetPrefix -ErrorAction Stop
+    $vnet = New-AzVirtualNetwork -Name $networkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $addressPrefix -Subnet $subnet -ErrorAction Stop
 }
 $subnet = $vnet.Subnets.Where({$_.Name -eq "default"})
 
 $networkSecurityGroupName = $resourceGroupName+"-nsg"
 $nsg = Get-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue
-if($nsg -eq $null)
+if($null -eq $nsg)
 {
-    $sshRule = New-AzNetworkSecurityRuleConfig -Name ssh-rule -Description "Allow SSH" -Access Allow -Protocol Tcp -Direction Inbound -Priority 101 -SourceAddressPrefix Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22
-    $rdpRule = New-AzNetworkSecurityRuleConfig -Name rdp-rule -Description "Allow RDP" -Access Allow -Protocol Tcp -Direction Inbound -Priority 102 -SourceAddressPrefix Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389
-    $nsg = New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $sshRule,$rdpRule
+    $sshRule = New-AzNetworkSecurityRuleConfig -Name ssh-rule -Description "Allow SSH" -Access Allow -Protocol Tcp -Direction Inbound -Priority 101 -SourceAddressPrefix Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -ErrorAction Stop
+    $rdpRule = New-AzNetworkSecurityRuleConfig -Name rdp-rule -Description "Allow RDP" -Access Allow -Protocol Tcp -Direction Inbound -Priority 102 -SourceAddressPrefix Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389 -ErrorAction Stop
+    $nsg = New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $sshRule,$rdpRule -ErrorAction Stop
 }
 
 $publicIPAddressName = $vmName+"-pubip-"+$randomText
-$publicIP = New-AzPublicIpAddress -Name $publicIPAddressName -ResourceGroupName $resourceGroupName -Location $location -AllocationMethod Dynamic -DomainNameLabel $vmName
+$publicIP = New-AzPublicIpAddress -Name $publicIPAddressName -ResourceGroupName $resourceGroupName -Location $location -AllocationMethod Dynamic -DomainNameLabel $vmName -ErrorAction Stop
 
 $nicName = $vmName+"-ni-"+$randomText
-$nic = New-AzNetworkInterface -Name $nicName -ResourceGroupName $resourceGroupName -Location $location -SubnetId $subnet.Id -PublicIpAddressId $publicIP.Id -NetworkSecurityGroupId $nsg.Id
+$nic = New-AzNetworkInterface -Name $nicName -ResourceGroupName $resourceGroupName -Location $location -SubnetId $subnet.Id -PublicIpAddressId $publicIP.Id -NetworkSecurityGroupId $nsg.Id -ErrorAction Stop
 
-$vm = New-AzVMConfig -VMName $vmName -VMSize $vMSize
+$vm = New-AzVMConfig -VMName $vmName -VMSize $vMSize -ErrorAction Stop
 if($type -eq "win10")
 {
     #https://docs.microsoft.com/en-us/azure/marketplace/cloud-partner-portal/virtual-machine/cpp-winrm-over-https
@@ -79,12 +88,12 @@ if($type -eq "win10")
     $vm = Set-AzVMOperatingSystem -VM $vm -Windows -ComputerName $vmName -Credential $vmCredentials -EnableAutoUpdate -TimeZone "Romance Standard Time" -WinRMHttp
     $vm = Set-AzVMSourceImage -VM $vm -PublisherName "MicrosoftWindowsDesktop" -Offer "Windows-10" -Skus "19h1-pron" -Version "latest"
 }
-if($type -eq "win7")
+elseif($type -eq "win7")
 {
     $vm = Set-AzVMOperatingSystem -VM $vm -Windows -ComputerName $vmName -Credential $vmCredentials -EnableAutoUpdate -TimeZone "Romance Standard Time"
     $vm = Set-AzVMSourceImage -VM $vm -PublisherName "MicrosoftWindowsDesktop" -Offer "windows-7" -Skus "win7-enterprise" -Version "latest"
 }
-if($type -eq "ubuntu")
+elseif($type -eq "ubuntu")
 {
     #https://docs.microsoft.com/en-us/azure/virtual-machines/linux/cli-ps-findimage
     $vm = Set-AzVMOperatingSystem -Linux -VM $vm -ComputerName $vmName -Credential $vmCredentials -DisablePasswordAuthentication
@@ -92,10 +101,16 @@ if($type -eq "ubuntu")
     Add-AzVMSshPublicKey -VM $vm -KeyData $sshPublicKey -Path "/home/$vmusername/.ssh/authorized_keys"
     #sudo timedatectl set-timezone Europe/Copenhagen
 }
-if($type -eq "server2016")
+elseif($type -eq "server2016")
 {
     $vm = Set-AzVMOperatingSystem -VM $vm -Windows -ComputerName $vmName -Credential $vmCredentials -EnableAutoUpdate -TimeZone "Romance Standard Time"
     $vm = Set-AzVMSourceImage -VM $vm -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer" -Skus "2016-Datacenter" -Version "latest"
+}
+elseif($type -eq "winsql2019")
+{
+    $vm = Set-AzVMOperatingSystem -VM $vm -Windows -ComputerName $vmName -Credential $vmCredentials -EnableAutoUpdate -TimeZone "Romance Standard Time"
+    $vm = Set-AzVMSourceImage -VM $vm -PublisherName "microsoftsqlserver" -Offer "sql2019-ws2019" -Skus "sqldev" -Version "latest"
+    
 }
 
 $vm = Set-AzVMBootDiagnostic -VM $vm -Disable
@@ -103,7 +118,7 @@ $vm = Add-AzVMNetworkInterface -VM $vm -Id $nic.Id
 $osDiskName = $vmName+"-osdisk-ssd"
 $vm = Set-AzVMOSDisk -VM $vm -Name $osDiskName -StorageAccountType StandardSSD_LRS -CreateOption FromImage
 
-New-AzVM -ResourceGroupName $resourceGroupName -Location $location -VM $vm
+New-AzVM -ResourceGroupName $resourceGroupName -Location $location -VM $vm -ErrorAction Stop
 
 $vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
 #Configure autoshutdown
@@ -116,7 +131,7 @@ $autoShutdownProp.Add('notificationSettings', @{status='Disabled'; timeInMinutes
 $autoShutdownProp.Add('targetResourceId', $vm.Id)
 
 $scheduledShutdownResourceId = $vm.Id.Replace("Microsoft.Compute", "microsoft.devtestlab").Replace("virtualMachines", "schedules").Replace($vmName,"shutdown-computevm-"+$vmName)
-New-AzResource -Location $location -ResourceId $scheduledShutdownResourceId -Properties $autoShutdownProp -Force
+New-AzResource -Location $location -ResourceId $scheduledShutdownResourceId -Properties $autoShutdownProp -Force -ErrorAction Stop
 
 if($stopVM -eq $true)
 {
